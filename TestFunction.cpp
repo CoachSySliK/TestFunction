6c6eeb7923f50cfd85a74ba445ef680a67205bb3
Юрий Бояджиев <boyadziev@mail.ru>
2024-04-12 20:13:45 +0300

    Добавьте файлы проекта.

---
+++ TestFunction.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+string rub(int ending) {
+    if (ending % 10 == 0 || ending % 10 >= 5 || (ending / 10) % 10 == 1) {
+        return " рублей";
+    }
+    else if (ending % 10 == 1) {
+        return " рубль";
+    }
+    else {
+        return " рубля";
+    }
+}
+
+int main()
+{
+    int x = 34;
+    cout << x << rub(x) << endl;
+    x = 35;
+    cout << x << rub(x) << endl;
+    x = 36;
+    cout << x << rub(x) << endl;
+    x = 128;
+    cout << x << rub(x) << endl;
+    x = 112;
+    cout << x << rub(x) << endl;
+    x = 300;
+    cout << x << rub(x) << endl;
+    x = 0;
+    cout << x << rub(x) << endl;
+    x = 51;
+    cout << x << rub(x) << endl;
+}
+
